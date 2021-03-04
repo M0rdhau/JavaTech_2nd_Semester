3e9086e6ea865c8da21cadeb613fc4c5eb6d4112
Luca Mizzi <lumizz@ttu.ee>
2021-03-04 22:39:35 +0200

    First commit, initialized spring

---
+++ src/main/java/ee/ttu/cs/team12jakarta/project2/CalculatorsApplication.java
@@ -0,0 +1,14 @@
+
+package ee.ttu.cs.team12jakarta.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/ttu/cs/team12jakarta/project2/CalculatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ttu.cs.team12jakarta.project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
