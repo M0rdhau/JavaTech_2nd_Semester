9065218f66ad7b47b876f21deea037c9e9733cf7
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-03-05 21:30:10 +0200

    Unit tests are all passing. We can work on making the code better a bit later. but it's time to write endpoint code now

--- src/main/java/ee/ttu/cs/team12jakarta/project2/utils/CalcUtils.java
+++ src/main/java/ee/ttu/cs/team12jakarta/project2/utils/CalcUtils.java
@@ -3,20 +3,35 @@ package ee.ttu.cs.team12jakarta.project2.utils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CalcUtils {
 
     //todo: write code for utils
     public static Integer maxOdd(List<Integer> input){
-        return 0;
+        try{
+            return input.stream().filter(num -> num % 2 != 0).max(Integer::compareTo).get();
+        }catch(Exception e){
+            return null;
+        }
     }
 
     public static Integer sum(List<Integer> input){
-        return 0;
+        try{
+            return input.stream().reduce(Integer::sum).get();
+        }catch(Exception e){
+            return null;
+        }
     }
 
     public static List<Integer> absolutes(List<Integer> input){
-        return List.of(0);
+        try{
+            List<Integer> collectedList = input.stream().map(e -> ((e < 0 ) ? e*(-1) : e)).collect(Collectors.toList());
+            if(collectedList.isEmpty()) return null;
+            return collectedList;
+        }catch(Exception e){
+            return null;
+        }
     }
 }
