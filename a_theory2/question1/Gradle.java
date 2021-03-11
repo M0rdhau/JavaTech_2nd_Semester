6a82f3c19942f63a6d6f132ee483d3ed9b7fcbe4
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-03-11 18:35:40 +0200

    My part of theory all done

--- a_theory2/question1/Gradle.java
+++ a_theory2/question1/Gradle.java
@@ -8,7 +8,9 @@ public class Gradle {
     //todo A
     // In your words (do not copy wiki definitions)
     // What is gradle?
-    // Answer:
+    // Answer: Gradle is a tool that bundles everything in the project, packages and
+    // all code - makes sure it all works and everything is linked.
+    // lets us run our code, as well as tests(which is also our code)
 
     //todo B
     // What is gradle wrapper? Why are we using it instead of gradle?
@@ -16,7 +18,7 @@ public class Gradle {
 
     //todo C
     // Name 3 different benefits or reasons for using gradle
-    // 1
+    // 1 It lets us synchronize code between packages
     // 2
     // 3
 
--- a_theory2/question3contribution/Feedback1.java
+++ a_theory2/question3contribution/Feedback1.java
@@ -10,21 +10,28 @@ public class Feedback1 {
 
     //todo A
     // On the scale 1 to 10 how useless or useful this course was with 1 being very useless and 10 being very useful?
-    //
+    // 9(can't give perfect 10)
 
     //todo B
     // Write some things you liked? At least 1, but you can write more.
-    //
-    //
+    // Course was short but very informative, a good entry point into Spring - if I didn't have
+    // 45 credits this semester, I'd surely switch my blog(that I wrote in Node.js for another
+    // subject) to Spring.
+    // Also I liked all the fantasy references during the lectures :)
 
     //todo C
     // Write some things you didn't like? At least 1, but you can write more.
-    //
-    //
+    // course was very short - by which I mean it was only 3 creds! If it were 6 credit
+    // course with extra Java stuff and some OOP sprinkled in, as well as some other web stuff
+    // I'd give it a perfect 10!
 
     //todo D
     // Anything else to add? Comments? What to do differently next year? (optional)
-    //
-    //
+    // I have this with all courses - if I had the opportunity to know everything in advance,
+    // I'd do it in the first week, especially since I knew some Java and some Web
+    // I think it will help students who aren't complete newbies to the topic
+    // and that have a lot of work on their hands, to have the ability to write everything
+    // in the first few days of the semester - given that they know the subject.
+    // Other than that, for a 3 cred course it's very much okay
 
 }
