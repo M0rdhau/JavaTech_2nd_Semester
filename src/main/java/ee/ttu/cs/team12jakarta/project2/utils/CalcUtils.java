a5c45fa9a983df785ee9fd89f03847e4109bab9f
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-03-18 21:15:31 +0200

    changed CalcUtils - description in the comments

--- src/main/java/ee/ttu/cs/team12jakarta/project2/utils/CalcUtils.java
+++ src/main/java/ee/ttu/cs/team12jakarta/project2/utils/CalcUtils.java
@@ -9,12 +9,28 @@ import java.util.Collection;
 @Service
 public class CalcUtils {
 
+    // Next two functions are written in an odd way, with a try-catch block surrounding them
+    // because originally, tests required that on null input the output be null.
+    // in the last commit I removed try/catch block because the CalculatorService was already
+    // dealing with null input - implementing the same approach as in the service looks like
+    // copy-pasted code to me, while try/catch just looks ugly.
+    // couldn't figure a better way to do it
     public static Integer maxOdd(List<Integer> input) {
-        return input.stream().filter(num -> num % 2 != 0).max(Integer::compareTo).get();
+        try{
+            return input.stream().filter(num -> num % 2 != 0).max(Integer::compareTo).get();
+        } catch(Exception e){
+            //log the exception
+            return null;
+        }
     }
 
     public static Integer sum(List<Integer> input) {
-        return input.stream().reduce(Integer::sum).get();
+        try{
+            return input.stream().reduce(Integer::sum).get();
+        } catch (Exception e){
+            //log the exception
+            return null;
+        }
     }
 
     public static List<Integer> absolutes(List<Integer> input) {
